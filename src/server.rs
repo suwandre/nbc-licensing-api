8fa8e56bffc9bf8df54ce42c66d93a173d390fba
suwandre <suwandresukijat@gmail.com>
2023-11-28 22:09:35 +0100

    feat: main -> server.rs; initialized salvo framework

---
+++ src/server.rs
@@ -0,0 +1,16 @@
+use salvo::prelude::*;
+
+/// Checks to see if Salvo is running
+#[handler]
+async fn run_salvo() -> &'static str {
+    "Salvo is running!"
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().init();
+
+    let router = Router::new().get(run_salvo);
+    let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
+    Server::new(acceptor).serve(router).await;
+}
