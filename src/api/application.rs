df60ab27823aa802d4aa2adcb86b8437f7f506a0
suwandre <suwandresukijat@gmail.com>
2023-12-04 12:04:25 +0100

    refactor: pack_data to use new bitpos and bitmask functions

--- src/api/application.rs
+++ src/api/application.rs
@@ -1,5 +1,7 @@
 use ethers::types::U256;
 
+use crate::utils::bitpos;
+
 use super::get_license_base_terms;
 
 /// Calculates the license fee for a license application.
@@ -39,46 +41,46 @@ pub async fn calculate_license_fee(
     Ok(U256::from(base_fee * duration_multiplier))
 }
 
-// /// Packs the application's parameters into `firstPackedData` and `secondPackedData`, as required by `IApplication.sol - ApplicationData`.
-// pub fn pack_data(
-//     duration: U256,
-//     license_fee: U256,
-//     reporting_frequency: U256,
-//     reporting_grace_period: U256,
-//     royalty_grace_period: U256,
-//     extra_data: U256
-// ) -> [U256; 2] {
-//     let current_timestamp = U256::from(chrono::Utc::now().timestamp());
-//     let submission_date = current_timestamp;
-//     let approval_date = U256::from(0);
-//     let expiration_date = current_timestamp + duration;
+/// Packs the application's parameters into `firstPackedData` and `secondPackedData`, as required by `IApplication.sol - ApplicationData`.
+pub fn pack_data(
+    duration: U256,
+    license_fee: U256,
+    reporting_frequency: U256,
+    reporting_grace_period: U256,
+    royalty_grace_period: U256,
+    extra_data: U256
+) -> [U256; 2] {
+    let current_timestamp = U256::from(chrono::Utc::now().timestamp());
+    let submission_date = current_timestamp;
+    let approval_date = U256::from(0);
+    let expiration_date = current_timestamp + duration;
 
-//     let untimely_reports = U256::from(0);
-//     let untimely_royalty_payments = U256::from(0);
+    let untimely_reports = U256::from(0);
+    let untimely_royalty_payments = U256::from(0);
 
-//     // bitposes:
-//     // [0 - 39] - the application's submission date.
-//     // [40 - 79] - the application's approval date (if approved; else 0).
-//     // [80 - 119] - the license's expiration date.
-//     // [120 - 255] - the license fee for this license.
-//     let first_packed_data = submission_date 
-//         | (approval_date << APPROVAL_DATE_BITPOS) 
-//         | (expiration_date << EXPIRATION_DATE_BITPOS) 
-//         | (license_fee << LICENSE_FEE_BITPOS);
+    // bitposes:
+    // [0 - 39] - the application's submission date.
+    // [40 - 79] - the application's approval date (if approved; else 0).
+    // [80 - 119] - the license's expiration date.
+    // [120 - 255] - the license fee for this license.
+    let first_packed_data = submission_date 
+        | (approval_date << bitpos("approval_date")) 
+        | (expiration_date << bitpos("expiration_date")) 
+        | (license_fee << bitpos("license_fee"));
 
-//     // bitposes:
-//     // [0 - 31] - the reporting frequency (frequency that a licensee must submit a revenue report and subsequently pay their royalty share).
-//     // [32 - 63] - the reporting grace period (amount of time given to report after the reporting frequency has passed before the licensee is penalized).
-//     // [64 - 95] - the royalty grace period (amount of time given to pay royalty after the revenue report for that period has been approved before the licensee is penalized).
-//     // [96 - 103] - the amount of untimely reports (i.e. reports that were not submitted within the reporting grace period).
-//     // [104 - 111] - the amount of untimely royalty payments (i.e. royalty payments that were not submitted within the royalty grace period).
-//     // [112 - 255] - extra data (if applicable).
-//     let second_packed_data = reporting_frequency 
-//         | (reporting_grace_period << REPORTING_GRACE_PERIOD_BITPOS) 
-//         | (royalty_grace_period << ROYALTY_GRACE_PERIOD_BITPOS) 
-//         | (untimely_reports << UNTIMELY_REPORTS_BITPOS)
-//         | (untimely_royalty_payments << UNTIMELY_ROYALTY_PAYMENTS_BITPOS)
-//         | (extra_data << EXTRA_DATA_BITPOS);
+    // bitposes:
+    // [0 - 31] - the reporting frequency (frequency that a licensee must submit a revenue report and subsequently pay their royalty share).
+    // [32 - 63] - the reporting grace period (amount of time given to report after the reporting frequency has passed before the licensee is penalized).
+    // [64 - 95] - the royalty grace period (amount of time given to pay royalty after the revenue report for that period has been approved before the licensee is penalized).
+    // [96 - 103] - the amount of untimely reports (i.e. reports that were not submitted within the reporting grace period).
+    // [104 - 111] - the amount of untimely royalty payments (i.e. royalty payments that were not submitted within the royalty grace period).
+    // [112 - 255] - extra data (if applicable).
+    let second_packed_data = reporting_frequency 
+        | (reporting_grace_period << bitpos("reporting_grace_period")) 
+        | (royalty_grace_period << bitpos("royalty_grace_period")) 
+        | (untimely_reports << bitpos("untimely_reports"))
+        | (untimely_royalty_payments << bitpos("untimely_royalty_payments")
+        | (extra_data << bitpos("extra_data")));
 
-//     [first_packed_data, second_packed_data]
-// }
+    [first_packed_data, second_packed_data]
+}
