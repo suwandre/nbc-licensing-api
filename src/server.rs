f7f1fd860e8295e0e09a72a6d254a2e1031c96a3
suwandre <suwandresukijat@gmail.com>
2023-12-05 12:45:02 +0100

    refactor: tcp listener ip

--- src/server.rs
+++ src/server.rs
@@ -24,6 +24,6 @@ async fn main() {
     load_env().await;
 
     let router = Router::new().get(run_salvo);
-    let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
+    let acceptor = TcpListener::new("0.0.0.0:5800").bind().await;
     Server::new(acceptor).serve(router).await;
 }
