4c3e6fabe5b6cc9e1f960d5dd316923cc22dde16
suwandre <suwandresukijat@gmail.com>
2023-12-12 21:40:27 +0100

    feat: User impl block and datetime option serialization

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,10 +1,12 @@
 use chrono::{DateTime, Utc};
 use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
-use crate::utils::serialization::datetime::{serialize_datetime, deserialize_datetime};
+use crate::{utils::serialization::datetime::{serialize_datetime, deserialize_datetime, serialize_datetime_option, deserialize_datetime_option}, configs::get_db};
 
 /// `User` struct that represents a user in the database.
-#[derive(Debug, Serialize, Deserialize)]
+/// 
+/// NOTE: Temporarily, no passwords are managed here as users are required to log in via their wallet.
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct User {
     /// the object ID of the user in the database
     pub _id: ObjectId,
@@ -19,17 +21,17 @@ pub struct User {
     
     /* web2 related info */
     /// the user's full name
-    pub name: String,
+    pub name: Option<String>,
     /// the user's date of birth
-    #[serde(serialize_with = "serialize_datetime", deserialize_with = "deserialize_datetime")]
-    pub dob: DateTime<Utc>,
+    #[serde(serialize_with = "serialize_datetime_option", deserialize_with = "deserialize_datetime_option")]
+    pub dob: Option<DateTime<Utc>>,
     /// the user's email address
-    pub email: String,
+    pub email: Option<String>,
     /// the user's phone number
-    pub phone: String,
+    pub phone: Option<String>,
     /// the user's main home/company address
-    pub address: String,
-    /// (optional) the user's company name
+    pub address: Option<String>,
+    /// the user's company name
     pub company: Option<String>,
     /// if the user has completed KYC
     pub kyc_verified: bool,
@@ -38,4 +40,42 @@ pub struct User {
     /// if the user has never completed KYC, this will be the same as `created_at`.
     #[serde(serialize_with = "serialize_datetime", deserialize_with = "deserialize_datetime")]
     pub last_kyc_verification: DateTime<Utc>,
+}
+
+impl User {
+    /// Creates a new User instance. 
+    /// 
+    /// NOTE: `new` is expected to be called when a user logs in via their wallet and doesn't have an existing account yet.
+    /// Hence, all Web2-related fields will be left blank.
+    pub fn new(wallet_address: String) -> Self {
+        Self {
+            _id: ObjectId::new(),
+            wallet_address,
+            created_at: Utc::now(),
+            updated_at: Utc::now(),
+            name: None,
+            dob: None,
+            email: None,
+            phone: None,
+            address: None,
+            company: None,
+            kyc_verified: false,
+            last_kyc_verification: Utc::now(),
+        }
+    }
+
+    /// Stores a User instance in the database.
+    /// 
+    /// Returns its newly created `ObjectId` if the operation is successful.
+    pub async fn store_user(&self) -> Result<ObjectId, mongodb::error::Error> {
+        let db = get_db("Licensing").await;
+        let user_col = db.collection::<Self>("Users");
+
+        let user = user_col.insert_one(self, None).await?;
+
+        match user.inserted_id.as_object_id() {
+            Some(id) => Ok(id),
+            None => Err(mongodb::error::Error::custom("Failed to get inserted ID."))
+        }
+    }
 }
--- src/utils/serialization/datetime.rs
+++ src/utils/serialization/datetime.rs
@@ -25,3 +25,38 @@ where
         .map_err(serde::de::Error::custom)
         .map(|dt| dt.with_timezone(&Utc))
 }
+
+/// Custom serialization for Option<DateTime<Utc>>
+pub fn serialize_datetime_option<S>(
+    date: &Option<DateTime<Utc>>,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    match date {
+        Some(date) => {
+            let formatted = date.to_rfc3339();
+            serializer.serialize_str(&formatted)
+        }
+        None => serializer.serialize_none(),
+    }
+}
+
+/// Custom deserialization for Option<DateTime<Utc>>
+pub fn deserialize_datetime_option<'de, D>(
+    deserializer: D,
+) -> Result<Option<DateTime<Utc>>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: Option<String> = Option::deserialize(deserializer)?;
+    match s {
+        Some(s) => {
+            DateTime::parse_from_rfc3339(&s)
+                .map(|dt| Some(dt.with_timezone(&Utc)))
+                .map_err(serde::de::Error::custom)
+        }
+        None => Ok(None),
+    }
+}
