ac2d94cb7f7ae3150f292c750f99277a1e63e159
suwandre <suwandresukijat@gmail.com>
2023-12-12 14:00:40 +0100

    chore: switched from salvo to axum

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: ./target/release/nbc-licensing-api
--- src/server.rs
+++ src/server.rs
@@ -3,27 +3,32 @@ use std::str::FromStr;
 use api::{get_license_base_terms, calculate_license_fee, pack_data};
 use chrono::{DateTime, Utc};
 use configs::load_env;
+use std::env;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use ethers::types::U256;
 use models::{Licensee, LicenseeRaw};
-use salvo::prelude::*;
+use axum::{routing::get, Router};
 
 mod api;
 mod configs;
 mod models;
 mod utils;
 
-/// Checks to see if Salvo is running.
-#[handler]
-async fn run_salvo() -> &'static str {
-    "Salvo is running!"
+/// Checks to see if Axum is running.
+async fn run_axum() -> &'static str {
+    "Axum is running!"
 }
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt().init();
-    load_env().await;
+    load_env();
 
-    let router = Router::new().get(run_salvo);
-    let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
-    Server::new(acceptor).serve(router).await;
+    let port = env::var("PORT").expect("PORT not set in .env");
+    let port = port.parse::<u16>().expect("Invalid port given");
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), port);
+
+    let app = Router::new().route("/", get(run_axum));
+
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
